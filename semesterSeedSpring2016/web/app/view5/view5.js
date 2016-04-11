eae68d63a3a0788bf180f34d517ffef7c528223d
mawmaw1 <kontakt@magnuslarsen.dk>
2016-04-11 18:49:15 +0200

    Day1 commit

--- semesterSeedSpring2016/web/app/view5/view5.js
+++ semesterSeedSpring2016/web/app/view5/view5.js
@@ -1,7 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+'use strict';
+
+angular.module('myApp.view5', ['ngRoute'])
+
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view5', {
+                    templateUrl: 'app/view5/view5.html',
+                    controller: 'View5Ctrl',
+                    controllerAs: 'ctrl'
+                });
+            }])
+
+        .controller('View5Ctrl', ["$http", "$location", function ($http, $location) {
+                var self = this;
+
+                self.adduser = function () {
+
+                    $http.post("api/data/create", self.user).success(function () {
+                        console.log(self.user);
+                        self.user = {};
+                        $location.path('/app/view1.html')
+                    });
+
+                };
+
+
+
+            }]);
 
 
