dc006feb8d17a1d6ba3cffaaabab3209979ae253
xNoga <k.noga@hotmail.com>
2016-04-12 09:49:17 +0200

    Small add

--- semesterSeedSpring2016/web/app/view2/view2.js
+++ semesterSeedSpring2016/web/app/view2/view2.js
@@ -5,18 +5,31 @@ angular.module('myApp.view2', ['ngRoute'])
         .config(['$routeProvider', function ($routeProvider) {
             $routeProvider.when('/view2', {
               templateUrl: 'app/view2/view2.html',
-              controller: 'View2Ctrl'
+              controller: 'View2Ctrl',
+              controllerAs: 'ctrl'
             });
           }])
 
-        .controller('View2Ctrl', function ($http, $scope) {
+        .controller('View2Ctrl', ["$http",function ($http) {
+                var self = this;
           $http({
             method: 'GET',
             url: 'api/demouser'
           }).then(function successCallback(res) {
-            $scope.data = res.data.message;
+            self.data = res.data.message;
           }, function errorCallback(res) {
-            $scope.error = res.status + ": "+ res.data.statusText;
+            self.error = res.status + ": "+ res.data.statusText;
           });
+          
+          self.moredata = [
+              {
+                  name: "hej",
+                  content: "Test fra hej"
+              },
+              {
+                  name: "hej 2",
+                  content: "Test fra hej 2"
+              }
+          ];
 
-        });
+        }]);
